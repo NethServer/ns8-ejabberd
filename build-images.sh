e7d6cb21a719e745ce5cca817d20d0c332bb5fe4
Stephane de Labrusse <stephdl@de-labrusse.fr>
2023-02-07 15:59:26 +0100

    Build script of ns8-ejabberd

--- build-images.sh
+++ build-images.sh
@@ -36,10 +36,10 @@ buildah add "${container}" imageroot /imageroot
 buildah add "${container}" ui/dist /ui
 # Setup the entrypoint, ask to reserve one TCP port with the label and set a rootless container
 buildah config --entrypoint=/ \
-    --label="org.nethserver.authorizations=traefik@node:routeadm" \
-    --label="org.nethserver.tcp-ports-demand=1" \
+    --label="org.nethserver.authorizations=traefik@node:routeadm traefik@node:certadm node:fwadm" \
+    --label="org.nethserver.tcp-ports-demand=0" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/jmalloc/echo-server:latest" \
+    --label="org.nethserver.images=docker.io/ejabberd/ecs:22.10" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
