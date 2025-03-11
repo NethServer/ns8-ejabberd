af9d3bbd97b06a7948a7fbe096ee1ad4dfeac080
Davide Principi <davide.principi@nethesis.it>
2025-03-11 14:23:26 +0100

    fix(build): renovate and test upgrade

--- test-module.sh
+++ test-module.sh
@@ -1,36 +1,49 @@
 #!/bin/bash
 
 #
-# Copyright (C) 2022 Nethesis S.r.l.
+# Copyright (C) 2025 Nethesis S.r.l.
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-LEADER_NODE=$1
-IMAGE_URL=$2
+#
+# Hint: access the test logs on HTTP port 8000 with this command:
+#
+#     python -mhttp.server -d tests/outputs/ 8000 &
+#
+
+set -e
+
 SSH_KEYFILE=${SSH_KEYFILE:-$HOME/.ssh/id_rsa}
 
-ssh_key="$(cat $SSH_KEYFILE)"
+LEADER_NODE="${1:?missing LEADER_NODE argument}"
+IMAGE_URL="${2:?missing IMAGE_URL argument}"
+shift 2
 
+ssh_key="$(< $SSH_KEYFILE)"
+
+cleanup() {
+    set +e
+    podman cp rf-core-runner:/home/pwuser/outputs tests/
+    podman stop rf-core-runner
+    podman rm rf-core-runner
+}
+
+trap cleanup EXIT
 podman run -i \
-    -v .:/home/pwuser/ns8-module:z \
-    --name rf-core-runner ghcr.io/marketsquare/robotframework-browser/rfbrowser-stable:v10.0.3 \
+    --network=host \
+    --volume=.:/home/pwuser/ns8-module:z \
+    --volume=site-packages:/home/pwuser/.local/lib/python3.12/site-packages:z \
+    --name rf-core-runner ghcr.io/marketsquare/robotframework-browser/rfbrowser-stable:19.1.2 \
     bash -l -s <<EOF
-    set -e
-    echo "$ssh_key" > /home/pwuser/ns8-key
-    set -x
-    pip install -r /home/pwuser/ns8-module/tests/pythonreq.txt
-    mkdir ~/outputs
-    cd /home/pwuser/ns8-module
-    robot -v NODE_ADDR:${LEADER_NODE} \
-        -v IMAGE_URL:${IMAGE_URL} \
-        -v SSH_KEYFILE:/home/pwuser/ns8-key \
-	-d ~/outputs /home/pwuser/ns8-module/tests/
+set -e
+echo "$ssh_key" > /home/pwuser/ns8-key
+pip install -q -r /home/pwuser/ns8-module/tests/pythonreq.txt
+mkdir ~/outputs
+cd /home/pwuser/ns8-module
+exec robot -v NODE_ADDR:${LEADER_NODE} \
+    -v IMAGE_URL:${IMAGE_URL} \
+    -v SSH_KEYFILE:/home/pwuser/ns8-key \
+    --name "$(basename $PWD)" \
+    --skiponfailure unstable \
+    -d ~/outputs ${@} /home/pwuser/ns8-module/tests/
 EOF
-
-tests_res=$?
-
-podman cp rf-core-runner:/home/pwuser/outputs tests/
-podman stop rf-core-runner
-podman rm rf-core-runner
-
-exit ${tests_res}
--- tests/__init__.robot
+++ tests/__init__.robot
@@ -1,5 +1,6 @@
 *** Settings ***
 Library           SSHLibrary
+Library           DateTime
 
 *** Variables ***
 ${SSH_KEYFILE}    %{HOME}/.ssh/id_ecdsa
@@ -8,8 +9,24 @@ ${SSH_KEYFILE}    %{HOME}/.ssh/id_ecdsa
 Connect to the node
     Open Connection   ${NODE_ADDR}
     Login With Public Key    root    ${SSH_KEYFILE}
+
+Wait until boot completes
     ${output} =    Execute Command    systemctl is-system-running  --wait
     Should Be True    '${output}' == 'running' or '${output}' == 'degraded'
 
+Save the journal begin timestamp
+    ${tsnow} =    Get Current Date    result_format=epoch
+    Set Global Variable    ${JOURNAL_SINCE}    ${tsnow}
+
+Collect the suite journal
+    Execute Command    journalctl -S @${JOURNAL_SINCE} >journal-dump.log
+    Get File    journal-dump.log    ${OUTPUT DIR}/journal-${SUITE NAME}.log
+
 *** Settings ***
-Suite Setup       Connect to the Node
+Suite Setup       Run Keywords
+                  ...    Connect to the Node
+                  ...    Wait until boot completes
+                  ...    Save the journal begin timestamp
+
+Suite Teardown    Run Keywords
+                  ...    Collect the suite journal
