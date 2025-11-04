f857d016fdfbcc9683e82c93913f10933ba1736f
Stephane de Labrusse <stephdl@de-labrusse.fr>
2025-11-04 12:25:42 +0100

    Revert "chore(deps): update ejabberd/ecs docker tag to v25.08 (#69)"
    This reverts commit 0d534128c63cf2a2f6af206a891ea944591eb443.

--- build-images.sh
+++ build-images.sh
@@ -41,7 +41,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.max-per-node=1" \
     --label="org.nethserver.tcp-ports-demand=0" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/ejabberd/ecs:25.08" \
+    --label="org.nethserver.images=docker.io/ejabberd/ecs:25.07" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
